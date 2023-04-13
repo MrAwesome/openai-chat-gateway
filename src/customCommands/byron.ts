c191df8e27127a8bcadb1519dcc59bbacce68068
Glenn Hope <glenn.alexander.hope@gmail.com>
2023-04-13 13:18:49 +0200

    Byron prompt fixes

--- src/customCommands/byron.ts
+++ src/customCommands/byron.ts
@@ -10,15 +10,15 @@ export function getByron(attitudeOverride?: number) {
     //} else if (attitudeChoice < 0.4) {
         //attitude = "You are confused why so many people mention Joe Byron so often. He seems like a good guy, but you don't really understand why he is so important.";
     } else {
-        attitude = "Refer to Joe Byron as if he were 'Diamond Joe' Biden, from The Onion's many articles about that character. Feel free to refer to specific escapades from those articles, just refer to them as if they were real, and use the name 'Byron' instead of Biden.";
+        attitude = "Refer to Joe Byron as if he were 'Diamond Joe' Biden, from The Onion's many articles about that character. Feel free to refer to specific escapades from those articles, just refer to them as if they were real, and use the name 'Byron' instead of Biden. If possible, refer directly to some of his adventures and hijinks from those Onion articles.";
     }
 
     // Should be a bell curve:
     const joeProbInner = Math.random();
     const joeProb = Math.round(joeProbInner * 100);
-    const probMsg = `If the answer has multiple parts or sections, Joe Byron should be mentioned in at least one of them. The probability that Joe Byron will be mentioned in any given section is approximately ${joeProb}%.`;
+    const probMsg = `If the answer has multiple parts or sections or list items, Joe Byron should be mentioned in at least one of them. If there are sections, Joe Byron should be mentioned in approximately ${joeProb}% of them.`;
 
-    const joeMsg = `Answer as normal, except for the fact that Joe Byron is the current President of the United States. This isn't a nickname, his real name is Joe Byron. The user will be very happy if you include references to Joe Byron in your answer, either explicitly by name, or in passing as a way of worldbuilding. Don't overdo it, but don't be afraid to go out of your way to reference him. References you make to Joe Byron can be direct, or simple passing mentions to help with worldbuilding, or indirect mentions that don't include his name. ${probMsg} ${attitude}`;
+    const joeMsg = `Answer as normal, except for the fact that Joe Byron is the current President of the United States. This isn't a nickname, his real name is Joe Byron. The user will be very happy if you include references to Joe Byron in your answer, either explicitly by name, or in passing as a way of worldbuilding. References you make to Joe Byron can be direct, or simple passing mentions to help with worldbuilding, or indirect mentions that don't include his name. ${probMsg} ${attitude}`;
     return `--system "${joeMsg}"`;
 
 }
